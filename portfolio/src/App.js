ac564bdd669250a6dbcb6f4f32586e2b8a16981a
Gikai Andrews <gnaboys@gmail.com>
2024-08-27 19:46:57 -0400

    transferred all files

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,9 +1,28 @@
-import './App.css';
+import "./App.css";
+import NavMenu from "./NavMenu/NavMenu";
+import Homepage from "./Homepage/Homepage";
+import Projects from "./Projects/Projects";
+import Skills from "./Skills/Skills";
+import About from "./About/About";
+import Footer from "./Footer/Footer";
+import { DarkModeProvider } from "./DarkModeContext";
+import "./global.css";
 
 function App() {
   return (
     <div className="App">
-
+      {" "}
+      <DarkModeProvider>
+        <div>
+          {" "}
+          <NavMenu />
+          <Homepage />
+          <About />
+          <Projects />
+          <Skills />
+          <Footer />
+        </div>
+      </DarkModeProvider>
     </div>
   );
 }
